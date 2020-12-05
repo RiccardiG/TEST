de535e498b59c1bea0638e3d7c01200cdcd644f2
RiccardiG <75377320+RiccardiG@users.noreply.github.com>
2020-12-05 17:10:12 +0100

    Update code.py
    Tested swap function

--- code.py
+++ code.py
@@ -6,3 +6,9 @@ input('Hello There')
 def swap(a, b):
     a, b = b, a
     return a, b
+
+
+a = int(input("Choose a value"))
+b = int(input("Choose another value"))
+a, b = swap(a, b)
+print(a, b, 'Have been swapped')
